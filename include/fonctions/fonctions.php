6e4b5ff06d58eb92825d1032fb6536caf6e919df
jeremie.samson76@gmail.com <jeremie.samson76@gmail.com@cf7a9618-22ff-13b7-76a9-c5c234dd71a8>
2013-07-08 16:01:13 +0000

    list fonctionel

--- include/fonctions/fonctions.php
+++ include/fonctions/fonctions.php
@@ -1,3 +1,34 @@
 <?php
+function generateSQL($variables, $dateDebut, $dateFin){
+	$variables[0] = strtolower($variables[0]);
+	$sql_select = "SELECT " .$variables[0]. ".datetime,";
+	
+	for($i=0 ; $i < count($variables) ; $i++){
+		$variables[$i] = strtolower($variables[$i]);
+		if ($i == count($variables) -1) $sql_select .= $variables[$i]. ".value AS " .$variables[$i]."_value";
+		else $sql_select .= $variables[$i]. ".value AS " .$variables[$i]."_value ,";
+	}
+	$sql_select .= " FROM " .$variables[0]. " ";
+		
+	for($i=1 ; $i < count($variables) ; $i++){
+		$sql_select .= "LEFT JOIN " .$variables[$i]. " ON " .$variables[$i].".datetime = " .$variables[0]. ".datetime ";
+	}
+			
+	$sql_select .= " WHERE " .$variables[0]. ".datetime >= '" .$dateDebut. "'";
+	$sql_select .= " AND " .$variables[0]. ".datetime <= '" .$dateFin. "'";
+	return $sql_select;
+}
 
+function getLastValue($variable, $dateDebut){
+	/*
+	SELECT datetime,data_cfp.value AS data_cfp_value
+	FROM data_cfp
+	WHERE data_cfp.datetime <  '2013/06/30 17:24:24'
+	AND data_cfp.value IS NOT NULL
+	ORDER BY timestamp DESC
+	LIMIT 0 , 1
+	*/
+	
+	
+}
 ?>
--- list_data.php
+++ list_data.php
@@ -5,6 +5,7 @@
 Site :
 http://stackoverflow.com/questions/2309801/how-to-submit-checkbox-values-with-php-post-method (variables)
 http://php.net/manual/fr/function.count.php (count)
+http://php.net/manual/fr/function.strtolower.php (lowercase)
 -->
 
 <?php
@@ -17,56 +18,46 @@ http://php.net/manual/fr/function.count.php (count)
 	</head>
 	
 <body>
+	<?php
+	$variables = $_POST['variables'];
+	$dateDebut = $_POST['dateDebut'];
+	$dateFin   = $_POST['dateFin'];
+	?>
+	
 	<center>
 		<h1>H2oTechs DataManager V1.0</h1>
 	</center>
 	
 	<table border="1">
 	<tr>
-		<td>ID</td>
-		<td>Datetime</td>
-		<td>Value</td>
-		<td>State</td>
-		<td>Timestamp</td>
+		<th>Datetime</th>
+		
+		<?php
+		foreach($variables as $variable)
+			echo "<th>" .$variable. "</th>";
+		?>
 	</tr>
 	
 	<?php
-	$variables = $_POST['variables'];
-	$dateDebut = $_POST['dateDebut'];
-	$dateFin   = $_POST['dateFin'];
-	
 	if (isset($variables) && isset($dateDebut) && isset($dateFin)){
 		if (is_array($variables)){
 			$connexion = new PDO('mysql:host='.$config['host'].';dbname='.$config['db'], $config['user'], $config['pass']);
-			
-			$sql_select = "SELECT * FROM ";
-
-			for($i=0 ; $i < count($variables) ; $i++){
-				if ($i != count($variables)) $sql_select .= strtolower($variables[$i] . ',');
-				else $sql_select .= strtolower($variables[$i]);
+			$sql_select = generateSQL($variables, $dateDebut, $dateFin);
+			$query_select = $connexion->prepare($sql_select);
+			$query_select->execute();
+			echo "<br>".$sql_select."<br><br>";
+			while($data=$query_select->fetch(PDO::FETCH_OBJ)){
+				echo "<tr>";
+					echo "<td>" .$data->datetime. "</td>";
+					foreach($variables as $variable){ 
+						$value = strtolower($variable . "_value");
+						echo "<td>" .$data->$value. "</td>";
+					}
+				echo "</tr>";
 			}
-			
-			
-			$sql_select = substr($sql_select, 0, -1);
-			echo $sql_select;
-			//$query_select_cfc = $connexion->prepare($sql_select_cfc);
-			//$query_select_cfc->execute();
-				
 		}
 	}
-	/*
-
-	while($data=$query_select_cfc->fetch(PDO::FETCH_ASSOC)){
-		echo "<tr>";
-		echo "<td>" .$data['id_cfc']. "</td>";
-		echo "<td>" .$data['datetime']. "</td>";
-		echo "<td>" .$data['value']. "</td>";
-		echo "<td>" .$data['state']. "</td>";
-		echo "<td>" .$data['timestamp']. "</td>";
-		echo "</tr>";	
-	}
-	*/
 	?>
-
+	
 	</table>
 	</body>
