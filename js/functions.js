43c7ac0330e7bffef5431e1a77886e390cf962c1
jeremie.samson76@gmail.com <jeremie.samson76@gmail.com@cf7a9618-22ff-13b7-76a9-c5c234dd71a8>
2013-07-08 13:45:26 +0000

    ajout page index et js

---
+++ js/functions.js
@@ -0,0 +1,16 @@
+
+			function selectAll(source){
+				checkboxes = document.getElementsByName('variables');
+	  		    for(var i=0, n=checkboxes.length;i<n;i++) {
+		  		    checkboxes[i].checked = source.checked;
+		  		}
+			}
+			
+			function selectPref(source){
+				//CFP
+				checkboxes = document.getElementsByName('variables');
+	  		    for(var i=0, n=checkboxes.length;i<n;i++) {
+	  		    	if(checkboxes[i].value == "data_CFP" || checkboxes[i].value == "data_PC")
+		  		    	checkboxes[i].checked = source.checked;
+		  		}
+			}
