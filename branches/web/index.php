fbe7b3957ddf7794076547e9b51e5694d6f48471
jeremie.samson76@gmail.com <jeremie.samson76@gmail.com@cf7a9618-22ff-13b7-76a9-c5c234dd71a8>
2013-07-09 12:30:40 +0000

    verification formulaire

--- branches/web/index.php
+++ branches/web/index.php
@@ -3,9 +3,10 @@
 @version 1
 
 Site :
-http://stackoverflow.com/questions/386281/how-to-implement-select-all-check-box-in-html
-http://www.tizag.com/javascriptT/javascript-string-compare.php
-http://tarruda.github.io/bootstrap-datetimepicker/
+http://stackoverflow.com/questions/386281/how-to-implement-select-all-check-box-in-html (checkboxes)
+http://www.tizag.com/javascriptT/javascript-string-compare.php (compare strings)
+http://tarruda.github.io/bootstrap-datetimepicker/ (datetimepicker)
+http://openweb.eu.org/articles/validation_formulaire (validation form)
 -->
 
 <?php
@@ -40,32 +41,40 @@ http://tarruda.github.io/bootstrap-datetimepicker/
 		?>
 		
 		<!-- Récupération des variables à sortir -->
-		<h5>Quelle donn&eacute;es voulez vous r&eacute;cup&eacute;rer ?</h5>
-		<form action="list_data.php" method="POST">
-			<input type="checkbox" onClick="selectAll(this)">Tout selectionner<br>
-			<input type="checkbox" onClick="selectPref(this)">Selectionner CFP<br><br>
-			
-			<?php
-				while($data=$query_select_variables->fetch(PDO::FETCH_ASSOC)){
-					echo '<input type="checkbox" name="variables[]" value="data_' .$data['label']. '">' .$data['label'];
-				}
-			?>
+		<form action="list_data.php" name="form" method="POST" onsubmit="return valider(this)">
+		<div id="datas">
+			<h5 title="Au moins une">*Quelle donn&eacute;es voulez vous r&eacute;cup&eacute;rer ?</h5> 
+			<div id="datas_tools">
+				<input type="checkbox" onClick="selectAll(this)">Tout selectionner<br>
+				<input type="checkbox" onClick="selectPref(this)">Selectionner CFP<br><br>
+			</div>
+			<font id="data_checked_error" color="#FF6469"></font>
+			<div id="datas_variables">
+				<?php
+					while($data=$query_select_variables->fetch(PDO::FETCH_ASSOC)){
+						echo '<input type="checkbox" name="variables[]" value="data_' .$data['label']. '">' .$data['label'];
+					}
+				?>
+			</div>
+		</div>
 			
 		<!-- Récupération de date et d'heure -->
-		<h5>Date et heure de d&eacute;but et de fin ?</h5>
-		Date de d&eacute;but :
-		<div id="datetimepickerDebut" class="input-append date">
-		<input type="text" name="dateDebut"></input>
-	      <span class="add-on">
-	        <i data-time-icon="icon-time" data-date-icon="icon-calendar"></i>
-	      </span>
-	    </div>
-	    Date de fin :
-	    <div id="datetimepickerFin" class="input-append date">
-	      <input type="text" name="dateFin"></input>
-	      <span class="add-on">
-	        <i data-time-icon="icon-time" data-date-icon="icon-calendar"></i>
-	      </span>
+		<div id="datetimes">
+			<h5>Date et heure de d&eacute;but et de fin ?</h5>
+			<div id="datetime_title">*Date de d&eacute;but : <font id="datetime_debut_error" color="#FF6469"></font></div>
+			<div id="datetimepickerDebut" class="input-append date">
+			<input type="text" name="dateDebut"></input>
+		      <span class="add-on">
+		        <i data-time-icon="icon-time" data-date-icon="icon-calendar"></i>
+		      </span>
+		    </div>
+		    <div id="datetime_title">*Date de fin : <font id="datetime_fin_error" color="#FF6469"></font></div>
+		    <div id="datetimepickerFin" class="input-append date">
+		      <input type="text" name="dateFin"></input>
+		      <span class="add-on">
+		        <i data-time-icon="icon-time" data-date-icon="icon-calendar"></i>
+		      </span>
+		    </div>
 	    </div>
 	    <script type="text/javascript">
 	    	$('#datetimepickerDebut').datetimepicker({ format: 'yyyy/MM/dd hh:mm:ss' });
--- branches/web/js/functions.js
+++ branches/web/js/functions.js
@@ -1,16 +1,83 @@
+/*
+ *@autor Jérémie Samson
+ *@version 1
+ * 
+ *Sites
+ *http://www.siteduzero.com/forum/sujet/verifier-une-date-au-format-jjmmaaaa-93090 (regex)
+ */
 
-			function selectAll(source){
-				checkboxes = document.getElementsByName('variables[]');
-	  		    for(var i=0, n=checkboxes.length;i<n;i++) {
-		  		    checkboxes[i].checked = source.checked;
-		  		}
-			}
+function selectAll(source){
+	checkboxes = document.getElementsByName('variables[]');
+	for(var i=0, n=checkboxes.length;i<n;i++) {
+		checkboxes[i].checked = source.checked;
+	}
+}
 			
-			function selectPref(source){
-				//CFP
-				checkboxes = document.getElementsByName('variables[]');
-	  		    for(var i=0, n=checkboxes.length;i<n;i++) {
-	  		    	if(checkboxes[i].value == "data_CFP" || checkboxes[i].value == "data_PC")
-		  		    	checkboxes[i].checked = source.checked;
-		  		}
-			}
+function selectPref(source){
+	//CFP
+	checkboxes = document.getElementsByName('variables[]');
+    for(var i=0, n=checkboxes.length;i<n;i++) {
+    	if(checkboxes[i].value == "data_CFP" || checkboxes[i].value == "data_PC")
+			checkboxes[i].checked = source.checked;
+	}
+}
+
+function valider(form){
+  var res = 0;
+  var regexDateTime = /^[2-9][0-9][0-9][0-9]\/[0-1][0-9]\/[0-3][0-9]\s[0-2][0-9]:[0-5][0-9]:[0-5][0-9]$/;
+   
+  //Verification sur le nombre d'arguments coché
+  var variables = document.getElementsByName("variables[]");
+  var nbChecked = 0;
+  for (var i=0 ; i< variables.length ; i++)
+  {
+  	if(variables.item(i).checked)
+  		nbChecked++;
+  }
+  if (nbChecked > 0 ){
+  	  res++;
+  	  document.getElementById("data_checked_error").innerHTML = "";
+  }
+  else{
+  	  document.getElementById("data_checked_error").innerHTML = "Il doit au moins y avoir une variable de coch&eacute;e.";
+  }
+   
+  //Verification sur la date de début
+  var dateDebut = form.elements['dateDebut'];
+  if(dateDebut.value != "") {
+	  var test = regexDateTime.test(dateDebut.value);
+	  if (test){
+	  	  res++;
+	  	  document.getElementById("datetime_debut_error").innerHTML = "";
+	  	  dateDebut.style.backgroundColor = '#FFFFFF';
+	  }
+	  else{
+		  document.getElementById("datetime_debut_error").innerHTML = "Le format de la date doit être : 'AAAA/MM/JJ HH:MM:SS'.";
+		  dateDebut.style.backgroundColor = '#FF6469';
+	  }
+  }else{
+  	  document.getElementById("datetime_debut_error").innerHTML = "La date doit &ecirc;tre remplit au format : 'AAAA/MM/JJ HH:MM:SS'.";
+	  dateDebut.style.backgroundColor = '#FF6469';
+  }
+  
+  //Même vérification sur la date de fin
+  var dateFin = form.elements['dateFin'];
+  if(dateFin.value != "") {
+	  var test = regexDateTime.test(dateFin.value);
+	  if (test){
+	   	  res++;
+	   	  document.getElementById("datetime_fin_error").innerHTML = "";
+	   	  dateFin.style.backgroundColor = '#FFFFFF';
+	  }
+	  else {
+		  document.getElementById("datetime_fin_error").innerHTML = "Le format de la date doit être : 'AAAA/MM/JJ HH:MM:SS'.";
+		  dateFin.style.backgroundColor = '#FF6469';
+	  }
+  }else{
+  	  document.getElementById("datetime_fin_error").innerHTML = "La date doit &ecirc;tre remplit au format : 'AAAA/MM/JJ HH:MM:SS'.";
+	  dateFin.style.backgroundColor = '#FF6469';
+  }
+
+  if (res == 3) return true;
+  else return false;
+}
