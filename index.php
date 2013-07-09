a0b5c8a8e219f7e211439b690e45517cb05d395e
jeremie.samson76@gmail.com <jeremie.samson76@gmail.com@cf7a9618-22ff-13b7-76a9-c5c234dd71a8>
2013-07-09 15:34:04 +0000

    ajout footer header

--- index.php
+++ index.php
@@ -28,12 +28,10 @@ http://openweb.eu.org/articles/validation_formulaire (validation form)
 
 	</head>
 	
-	<body>
-		<center>
-		<h1>H2oTechs DataManager V1.0</h1>
-		Bienvenue dans votre espace d'administration de base de donn&eacute;e.<br><br>
-		</center>
+	<body>	
 		<?php
+			include("header.html");
+			
 			$connexion = new PDO('mysql:host='.$config['host'].';dbname='.$config['db'], $config['user'], $config['pass']);
 			$sql_select_variables = "SELECT * FROM variables";
 			$query_select_variables = $connexion->prepare($sql_select_variables);
@@ -41,65 +39,67 @@ http://openweb.eu.org/articles/validation_formulaire (validation form)
 		?>
 		
 		<!-- Récupération des variables à sortir -->
-		<form action="list_data.php" name="form" method="POST" onsubmit="return valider(this)">
-		<div id="variables">
-			<h5 title="Au moins une">*Quelle donn&eacute;es voulez vous r&eacute;cup&eacute;rer ?</h5> 
-			<div id="variables_tools">
-				<input type="radio" name="tools" onClick="selectAll(this)"> Tout selectionner<br>
-				<input type="radio" name="tools" onClick="selectPref(this)"> Selectionner CFP<br><br>
-			</div>
-			<font id="variables_checked_error"></font>
-			<div id="variables_header">
-				<table id="tabVariables" cellpadding="5">
-					<?php
-						//Creation du tableau de variables
-						$unite = "";
-						$pair = 1;
-						while($data=$query_select_variables->fetch(PDO::FETCH_ASSOC)){
-							$trColor = ($pair%2) ?  "id='ligne_impair'" : "id='ligne_pair'";
-							if ($data['unite'] != $unite) {
-								$pair++;
-								if ($unite == "") echo "<tr $trColor>";
-								else echo "</tr><tr $trColor>";
-
-								$unite = verifExposant($data['unite']);
-								echo '<td id="tabVariablesHeader"><center>'.$unite.'</center></td>';
-								echo '<td><input type="checkbox" name="variables[]" value="data_' .$data['label']. '">' .$data['label'] .'</td>';
+		<div id="corp">
+			<form action="list_data.php" name="form" method="POST" onsubmit="return valider(this)">
+			<div id="variables">
+				<h5 title="Au moins une">*Quelle donn&eacute;es voulez vous r&eacute;cup&eacute;rer ?</h5> 
+				<div id="variables_tools">
+					<input type="radio" name="tools" onClick="selectAll(this)"> Tout s&eacute;lectionner<br>
+					<input type="radio" name="tools" onClick="selectPref(this)"> S&eacute;lectionner CFP<br><br>
+				</div>
+				<font id="variables_checked_error"></font>
+				<div id="variables_header">
+					<table id="tabVariables" cellpadding="5">
+						<?php
+							//Creation du tableau de variables
+							$unite = "";
+							$pair = 1;
+							while($data=$query_select_variables->fetch(PDO::FETCH_ASSOC)){
+								$trColor = ($pair%2) ?  "id='ligne_impair'" : "id='ligne_pair'";
+								if ($data['unite'] != $unite) {
+									$pair++;
+									if ($unite == "") echo "<tr $trColor>";
+									else echo "</tr><tr $trColor>";
+	
+									$unite = verifExposant($data['unite']);
+									echo '<td id="tabVariablesHeader"><center>'.$unite.'</center></td>';
+									echo '<td title="' .$data['description']. '"><input type="checkbox" name="variables[]" value="data_' .$data['label']. '">' .$data['label'] .'</td>';
+								}
+								else
+									echo '<td title="' .$data['description']. '"><input type="checkbox" name="variables[]" value="data_' .$data['label']. '">' .$data['label'] .'</td>';	
 							}
-							else
-								echo '<td><input type="checkbox" name="variables[]" value="data_' .$data['label']. '">' .$data['label'] .'</td>';	
-						}
-					?>
-				</tr>
-				</table>
+						?>
+					</tr>
+					</table>
+				</div>
 			</div>
-		</div>
-			
-		<!-- Récupération de date et d'heure -->
-		<div id="datetimes">
-			<h5>Date et heure de d&eacute;but et de fin ?</h5>
-			<div id="datetime_title">*Date de d&eacute;but : <font id="datetime_debut_error"></font></div>
-			<div id="datetimepickerDebut" class="input-append date">
-			<input type="text" id="bouttonDate" name="dateDebut"></input>
-		      <span class="add-on">
-		        <i data-time-icon="icon-time" data-date-icon="icon-calendar"></i>
-		      </span>
-		    </div>
-		    <div id="datetime_title">*Date de fin : <font id="datetime_fin_error"></font></div>
-		    <div id="datetimepickerFin" class="input-append date">
-		      <input type="text" id="bouttonDate" name="dateFin"></input>
-		      <span class="add-on">
-		        <i data-time-icon="icon-time" data-date-icon="icon-calendar"></i>
-		      </span>
-		    </div>
-	    </div>
-	    <script type="text/javascript">
-	    	$('#datetimepickerDebut').datetimepicker({ format: 'yyyy/MM/dd hh:mm:ss' });
-	        $('#datetimepickerFin').datetimepicker({ format: 'yyyy/MM/dd hh:mm:ss' });
-	    </script>
-	    
-	    <br><br><input type="submit" value="Envoyer">
-		</form>
 		
+			<!-- Récupération de date et d'heure -->
+			<div id="datetimes">
+				<h5>Date et heure de d&eacute;but et de fin ?</h5>
+				<div id="datetime_title">*Date de d&eacute;but : <font id="datetime_debut_error"></font></div>
+				<div id="datetimepickerDebut" class="input-append date">
+				<input type="text" id="bouttonDate" name="dateDebut"></input>
+			      <span class="add-on">
+			        <i data-time-icon="icon-time" data-date-icon="icon-calendar"></i>
+			      </span>
+			    </div>
+			    <div id="datetime_title">*Date de fin : <font id="datetime_fin_error"></font></div>
+			    <div id="datetimepickerFin" class="input-append date">
+			      <input type="text" id="bouttonDate" name="dateFin"></input>
+			      <span class="add-on">
+			        <i data-time-icon="icon-time" data-date-icon="icon-calendar"></i>
+			      </span>
+			    </div>
+		    </div>
+		    <script type="text/javascript">
+		    	$('#datetimepickerDebut').datetimepicker({ format: 'yyyy/MM/dd hh:mm:ss' });
+		        $('#datetimepickerFin').datetimepicker({ format: 'yyyy/MM/dd hh:mm:ss' });
+		    </script>
+		    
+		    <br><br><input type="submit" value="Envoyer">
+			</form>
+		</div>
+		<?php include("footer.html"); ?>
 	</body>
 </html>
--- list_data.php
+++ list_data.php
@@ -10,62 +10,63 @@ http://php.net/manual/fr/function.strtolower.php (lowercase)
 
 <?php
 	include("include/include.php");
-?>
-
-<html>
-	<head>
-		<title>H2otechs DataManager</title>
-	</head>
-	
-<body>
-	<center>
-		<h1>H2oTechs DataManager V1.0</h1>
-	</center>
-	
-	<?php
 	$variables = $_POST['variables'];
 	$dateDebut = $_POST['dateDebut'];
 	$dateFin   = $_POST['dateFin'];
 	
 	//Test sur les différentes variables récupérées
 	if (isset($variables) && isset($dateDebut) && isset($dateFin)){
-		if (is_array($variables)){
-			//Connexion à la base de données
-			$connexion = new PDO('mysql:host='.$config['host'].';dbname='.$config['db'], $config['user'], $config['pass']);
-		 	?>
-		 	
-		 	<!-- Création du tableau et de son header-->
-		 	<table border="1">
-			<tr>
-				<th title="Temps au format AAAA/MM/JJ HH:MM:SS de la prise de donn&eacute;e">Datetime</th>
+		if (is_array($variables)){ ?>
+			<html>
+				<head>
+					<title>H2otechs DataManager</title>
+					<link rel="stylesheet" href="css/style.css">
+				</head>
 				
-				<?php
-				foreach($variables as $variable){
-					$variable = getHeader($variable);
-					echo "<th title='" .getDescriptionOfLabel($variable, $connexion). "'>" .$variable. "</th>";
-				}
-				?>
-			</tr>
-		 	
-		 	<?php
-		 	//Création de la requête et génération du tableau
-			$sql_select = generateSQL($variables, $dateDebut, $dateFin);
-			$query_select = $connexion->prepare($sql_select);
-			$query_select->execute();
+				<body>
+					<?php include("header.html");
+					
+					echo "<div id='corp'>";
 			
-			echo "<br>".$sql_select."<br><br>";
-			while($data=$query_select->fetch(PDO::FETCH_OBJ)){
-				echo "<tr>";
-					echo "<td>" .$data->datetime. "</td>";
-					foreach($variables as $variable){ 
-						$value = strtolower($variable . "_value");
-						echo "<td>" .$data->$value. "</td>";
-					}
-				echo "</tr>";
+						//Connexion à la base de données
+						$connexion = new PDO('mysql:host='.$config['host'].';dbname='.$config['db'], $config['user'], $config['pass']);
+						?>
+								 	
+						<!-- Création du tableau et de son header-->
+						<table border="1">
+							<tr>
+								<th title="Temps au format AAAA/MM/JJ HH:MM:SS de la prise de donn&eacute;e">Datetime</th>
+											
+								<?php
+									foreach($variables as $variable){
+										$variable = getHeader($variable);
+										echo "<th title='" .getDescriptionOfLabel($variable, $connexion). "'>" .$variable. "</th>";
+									}
+							echo "</tr>";
+									 
+							//Création de la requête et génération du tableau
+							$sql_select = generateSQL($variables, $dateDebut, $dateFin);
+							$query_select = $connexion->prepare($sql_select);
+							$query_select->execute();
+										
+							echo "<br>".$sql_select."<br><br>";
+							while($data=$query_select->fetch(PDO::FETCH_OBJ)){
+								echo "<tr>";
+									echo "<td>" .$data->datetime. "</td>";
+									foreach($variables as $variable){ 
+										$value = strtolower($variable . "_value");
+										echo "<td>" .$data->$value. "</td>";
+									}
+								echo "</tr>";
+							}
+						echo "</table>";
+					echo "</div>";
+					include("footer.html");
 			}
-		}
 	}
+	else
+		echo '<META HTTP-EQUIV="Refresh" CONTENT="0;URL=index.php">';
 	?>
 	
-	</table>
 	</body>
+</html>
