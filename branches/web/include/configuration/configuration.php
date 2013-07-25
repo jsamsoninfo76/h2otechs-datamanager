b9b534b4ed438d74a7865a6a5aa9604934a4b879
Jeremie.Samson76@gmail.com <Jeremie.Samson76@gmail.com@cf7a9618-22ff-13b7-76a9-c5c234dd71a8>
2013-07-25 20:11:53 +0000

    modif totalisateur pour enky4

--- branches/web/include/configuration/configuration.php
+++ branches/web/include/configuration/configuration.php
@@ -5,7 +5,19 @@ $config = array(
 	"user" => "root",
 	"pass" => "root",
 	"port" => "8889", 
-	"db"   => "h2otechs_datamanager"
+	"db"   => "h2otechs_datamanager_enky4",
+	"enky" => "4"
 );
+
+/*
+$config = array(
+	"host" => "localhost",
+	"user" => "root",
+	"pass" => "root",
+	"port" => "8889", 
+	"db"   => "h2otechs_datamanager_enky2",
+	"enky" => "2"
+);
+*/
 
 ?>
