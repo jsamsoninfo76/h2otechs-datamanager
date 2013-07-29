cec73ccd71325544eecfc241f59f218e436acc53
Jeremie.Samson76@gmail.com <Jeremie.Samson76@gmail.com@cf7a9618-22ff-13b7-76a9-c5c234dd71a8>
2013-07-29 14:35:02 +0000

    Ajout modification observation

--- branches/web/list_interventions.php
+++ branches/web/list_interventions.php
@@ -1,17 +1,35 @@
+<?php
+/**
+ * list_interventions.php
+ *
+ * Liste toutes les interventions avec un select en onclick:submit et un champ textarea pour afficher l'observation
+ * Possibilité de modifier l'observation 
+ * Inclut l'ajout d'intervention
+ *
+ */
+?>
+
 <div id='intervention'>
 	<!-- Retour de l'insertion qui vient du formulaire -->
 	<div id='retour_insertion'>
 		<?php
 			$connexion = new PDO('mysql:host='.$config['host'].';dbname='.$config['db'], $config['user'], $config['pass']);
 			
+			print_r($_GET);
+			
 			//Recuperation des variables
 			if (isset($_POST['dateIntervention'])) $dateintervention= $_POST['dateIntervention'];
 			if (isset($_POST['intervenant'])) $intervenant			= $_POST['intervenant'];
 			if (isset($_POST['observation'])) $observation			= $_POST['observation'];
+			if (isset($_POST['id_intervention'])) $id_intervention  = $_POST['id_intervention'];
 			
 			//Test sur les variables
-			if (!empty($dateintervention) && !empty($intervenant) && !empty($observation)){
-				
+			if (!empty($id_intervention) && !empty($observation)){
+				$sql_update = generateUpdateIntervention($id_intervention, $observation);
+				$query_update = $connexion->prepare($sql_update);
+				$query_update->execute();
+			}
+			else if (!empty($dateintervention) && !empty($intervenant) && !empty($observation)){
 				//Préparation de la requete SQL
 				$sql_insert = generateInsertIntervention($dateintervention, $intervenant, $observation);
 				$query_insert = $connexion->prepare($sql_insert);
@@ -22,24 +40,29 @@
 					echo "<font class='message_info'>L'intervention &agrave; bien &eacute;t&eacute; ajout&eacute;.</font>";
 				}
 				else
-					echo "<font class='message_error'>probleme insertion</font>";
+					echo "<font class='message_error'>Probleme lors de l'insertion de l'intervention</font>";
 				echo "<br/><br/>";
 			}
 		?>
 	</div>
 	
-	<!-- Creation du bouton back et next -->
 	<?php
-		if (isset($_POST['datetime'])) $datetime = (!empty($_POST['datetime'])) ? $_POST['datetime'] : $_GET['datetime'];
-		else $datetime = "";
+		//Déclaration de $datetime pour éviter les php_notices
+		if (isset($_POST['datetime']) || isset($_GET['datetime'])) 
+			$datetime = (!empty($_POST['datetime'])) ? $_POST['datetime'] : $_GET['datetime'];
+		else
+			$datetime = "";
+		
 	?>
 	
 	<!-- Creation du formulaire de l'intervention -->
 	<form name='formInterventions' method="post" action="index.php?id_page=4">
 		<div id='intervention_title'><h5>Date et Intervenant :</h5></div>
 		<a href='index.php?id_page=4&action=back&datetime=<?php echo getNextOrPrecDatetime($datetime, "back", $connexion);?>'><----&nbsp;&nbsp;</a>
+		
 	<select name='datetime' id='selectDatetime' onChange='this.form.submit()'>
 		<?php
+			//Création du select dynamique
 			$sql_select_interventions = generateInterventionsSQL();
 			$query_select_interventions = $connexion->prepare($sql_select_interventions);
 			$query_select_interventions->execute();
@@ -84,32 +107,35 @@
 		?>
 		</select>
 		<a href='index.php?id_page=4&action=next&datetime=<?php echo getNextOrPrecDatetime($datetime, "next", $connexion);?>'>&nbsp;&nbsp;----></a>
+	
+		<?php
+			//Déclaration du datetime pour éviter le php_notices
+			if (isset($_POST['datetime']) && !empty($_POST['datetime'])) $datetime = $_POST['datetime'];
+			else if (isset($_GET['datetime']) && !empty($_GET['datetime'])) $datetime = $_GET['datetime'];
+			else $datetime = "";
+			
+			//Récupération de l'intervention selectionné ou passer en paramètre
+			$sql_select_intervention = generateInterventionSQL($datetime); 
+			$query_select_intervention = $connexion->prepare($sql_select_intervention);
+			$query_select_intervention->execute();
+			$data = $query_select_intervention->fetch(PDO::FETCH_ASSOC);
+			$observation = $data['observation'];	
+			
+			echo "<input type='hidden' name='id_intervention' value='" .$data['id_intervention']. "'>";
+		?>
+	
+		<div class="observationBlock">
+			<h5>Observation :</h5>		
+			<textarea id="observation" name="observation" rows="10" cols="50"><?php echo $observation; ?></textarea>
+		</div>
+		
+		<input type="submit" value="Modifier">
+	
+		<script type="text/javascript">
+			$(document).ready(function() {
+			  $('#observation').elastic();
+			});
+		</script>
 	</form>
-	
-	<?php
-	//echo "<br/>".$sql_select_interventions."</br>";
-	
-	
-	if (isset($_POST['datetime']) && !empty($_POST['datetime'])) $datetime = $_POST['datetime'];
-	else if (isset($_GET['datetime']) && !empty($_GET['datetime'])) $datetime = $_GET['datetime'];
-	else $datetime = "";
-	
-	$sql_select_interventions = generateInterventionSQL($datetime); 
-	$query_select_interventions = $connexion->prepare($sql_select_interventions);
-	$query_select_interventions->execute();
-	$data = $query_select_interventions->fetch(PDO::FETCH_ASSOC);
-	$observation = $data['observation'];	
-
-	?>
-
-	<div class="observationBlock">
-		<h5>Observation :</h5>		
-		<textarea id="observation" rows="10" cols="50"><?php echo $observation; ?></textarea>
-	</div>
-	<script type="text/javascript">
-		$(document).ready(function() {
-		  $('#observation').elastic();
-		});
-	</script>
 </div>
 
