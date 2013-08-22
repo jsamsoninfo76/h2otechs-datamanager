f70aed3f419c37c0529c6b22a0d5c6e415ce5b3a
Jeremie.Samson76@gmail.com <Jeremie.Samson76@gmail.com@cf7a9618-22ff-13b7-76a9-c5c234dd71a8>
2013-08-22 15:27:56 +0000

    ajout export pdf

--- branches/web/pdf.php
+++ branches/web/pdf.php
@@ -26,22 +26,21 @@
  */
 
 /** Error reporting */
-error_reporting(E_ALL);
-ini_set('display_errors', TRUE);
-ini_set('display_startup_errors', TRUE);
+//error_reporting(E_ALL);
+//ini_set('display_errors', TRUE);
+//ini_set('display_startup_errors', TRUE);
 date_default_timezone_set('Europe/London');
 
 define('EOL',(PHP_SAPI == 'cli') ? PHP_EOL : '<br />');
 
 date_default_timezone_set('Europe/London');
 
-include "excelForPDF.php";
+$_SESSION['decalagepdf'] = 4;
+include "excel.php";
 
 /** PHPExcel_IOFactory */
 require_once 'include/include.php';
 
-$path = "upload/";
-$filename = "test.pdf";
 
 //filename & Path
 $path = "upload/";
@@ -50,22 +49,18 @@ $filename = $_SESSION['dateDebut'] ."_au_". $_SESSION['dateFin'] ."_". $_SESSION
 //	Change these values to select the Rendering library that you wish to use
 //		and its directory location on your server
 $rendererName = PHPExcel_Settings::PDF_RENDERER_TCPDF;
-//$rendererName = PHPExcel_Settings::PDF_RENDERER_MPDF;
-//$rendererName = PHPExcel_Settings::PDF_RENDERER_DOMPDF;
 $rendererLibrary = '';
-//$rendererLibrary = 'mPDF5.4';
-//$rendererLibrary = 'domPDF0.6.0beta3';
 $rendererLibraryPath = 'include/utile/tcpdf/' . $rendererLibrary;
 
 
-echo date('H:i:s') , " Hide grid lines" , EOL;
+if ($verbose) echo date('H:i:s') , " Hide grid lines" , EOL;
 $objPHPExcel->getActiveSheet()->setShowGridLines(false);
 
-echo date('H:i:s') , " Set orientation to landscape" , EOL;
+//if ($verbose) echo date('H:i:s') , " Set orientation to landscape" , EOL;
 //$objPHPExcel->getActiveSheet()->getPageSetup()->setOrientation(PHPExcel_Worksheet_PageSetup::ORIENTATION_LANDSCAPE);
 
 
-echo date('H:i:s') , " Write to PDF format using {$rendererName}" , EOL;
+if ($verbose) echo date('H:i:s') , " Write to PDF format using {$rendererName}" , EOL;
 
 if (!PHPExcel_Settings::setPdfRenderer(
 		$rendererName,
@@ -86,19 +81,19 @@ $objWriter->setSheetIndex(0);
 $objWriter->save($path . $filename);
 $callEndTime = microtime(true);
 $callTime = $callEndTime - $callStartTime;
-echo date('H:i:s') , " File written to " , str_replace('.php', '_'.$rendererName.'.pdf', pathinfo(__FILE__, PATHINFO_BASENAME)) , EOL;
-echo 'Call time to write Workbook was ' , sprintf('%.4f',$callTime) , " seconds" , EOL;
-// Echo memory usage
-echo date('H:i:s') , ' Current memory usage: ' , (memory_get_usage(true) / 1024 / 1024) , " MB" , EOL;
+if ($verbose) echo date('H:i:s') , " File written to " , str_replace('.php', '_'.$rendererName.'.pdf', pathinfo(__FILE__, PATHINFO_BASENAME)) , EOL;
+if ($verbose) echo 'Call time to write Workbook was ' , sprintf('%.4f',$callTime) , " seconds" , EOL;
+// if ($verbose) echo memory usage
+if ($verbose) echo date('H:i:s') , ' Current memory usage: ' , (memory_get_usage(true) / 1024 / 1024) , " MB" , EOL;
 
 
-// Echo memory peak usage
-echo date('H:i:s') , " Peak memory usage: " , (memory_get_peak_usage(true) / 1024 / 1024) , " MB" , EOL;
+// if ($verbose) echo memory peak usage
+if ($verbose) echo date('H:i:s') , " Peak memory usage: " , (memory_get_peak_usage(true) / 1024 / 1024) , " MB" , EOL;
 
-// Echo done
-echo date('H:i:s') , " Done writing files" , EOL;
-echo 'File has been created in ' , getcwd() , EOL;
-echo '<br/><a href="upload/test.pdf">Cliquez ici si le t&eacute;l&eacute;chargement ne commence pas</a>';
-
+// if ($verbose) echo done
+if ($verbose) echo date('H:i:s') , " Done writing files" , EOL;
+if ($verbose) echo 'File has been created in ' , getcwd() , EOL;
+//echo '<br/><a href="upload/' .$filename. '">Voir le document PDF</a>';
 echo "<META HTTP-EQUIV='Refresh' CONTENT='0;URL=downloadfile.php?filename=$filename'>";
+echo "<META HTTP-EQUIV='Refresh' CONTENT='0;URL=upload/" .$filename . "'>";
 ?>
